cee5d3d688ed3833b0024dbb7dfa593414aafbdf
Luis-Betancourt93 <betancourtluis619@gmail.com>
2025-02-17 20:05:08 -0800

    Install react router dom

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,10 +1,10 @@
 import { Client, Account} from 'appwrite';
-
+import { PROJECT_ID, DATABASE_ID, COLLECTION_ID } from './shhh.js';
 export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('<PROJECT_ID>'); // Replace with your project ID
+    .setProject(PROJECT_ID); // Replace with your project ID
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
